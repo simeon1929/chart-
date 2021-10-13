17d1a232ead997201423a5ac8336eb3cdffa5cb0
simeon1929 <simeon1929@gmail.com>
2021-10-13 14:56:51 +0530

    wave graph

--- waweChart.js
+++ waweChart.js
@@ -7,32 +7,48 @@ var commonOptins = {
         }
     },
     scales: {
-        // y: {
-        //     beginAtZero: true,
-        //     display:false
-        // },  
+        y: {
+            beginAtZero: true,
+            display:false
+        },  
         x: {
             beginAtZero: true,
             display:false
         }
+    },
+    elements: {
+        line: {
+            tension: .4
+        },
+        point:{
+            radius: 0
+        }
     }
 }
+
+// tension Curve 
+
+
+
 var dataValues = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['a', 'b', 'c', 'd', 'e', 'f' ,'g', 'h','i' ],
         datasets: [{
             label: '# of Votes',
-            data: [8, 19, 17, 3, 11, 18],
-            //barPercentage:-50,
-            barThickness: 4,// bar thickness
-            maxBarThickness: 4, // bar thickness
+            data: [3,5,12,2,8,12,3,12,6],
+            // //barPercentage:-50,
+            // barThickness: 4,// bar thickness
+            // maxBarThickness: 4, // bar thickness
            // minBarLength: 1,
-            borderWidth: 1,
+            borderWidth: 2,
             backgroundColor: [
                 'rgba(221, 101, 250, 1)',
                 'rgba(196, 171, 223, 1)',
                 'rgba(221, 101, 250, 1)',
                 'rgba(196, 171, 223, 1)',
                 'rgba(221, 101, 250, 1)',
+                'rgba(196, 171, 223, 1)',
+                'rgba(196, 171, 223, 1)',
+                'rgba(196, 171, 223, 1)',
                 'rgba(196, 171, 223, 1)',
             ],
             borderColor: [
@@ -42,9 +58,13 @@ var dataValues = {
                 'rgba(196, 171, 223, 1)',
                 'rgba(221, 101, 250, 1)',
                 'rgba(196, 171, 223, 1)',
+                'rgba(196, 171, 223, 1)',
+                'rgba(196, 171, 223, 1)',
+                'rgba(196, 171, 223, 1)',
             ],
         }]
 }
+
 var topSles = new Chart(ctxtopSles, {
     type: 'line',
     data: dataValues,
